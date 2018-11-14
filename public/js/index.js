60e38bcdb9527b5db129806ef06eb819c044b431
sabrina.moraru <sabrina.moraru@gmail.com>
2018-11-14 10:29:39 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on("connect", function() {
   console.log("Connected to server");
 
-  socket.emit("createMessage", {
-    from: "Sabrina",
-    text: "Yup, that works for me."
-  });
+  // socket.emit("createMessage", {
+  //   from: "Sabrina",
+  //   text: "Yup, that works for me."
+  // });
 });
 
 // socket.emit("createEmail", {
--- server/server.js
+++ server/server.js
@@ -20,14 +20,19 @@ io.on("connection", socket => {
   //   createdAt: 123
   // });
 
-  socket.emit("newMessage", {
-    from: "Alin",
-    text: "One more minute",
-    createdAt: 123123
-  });
+  // socket.emit("newMessage", {
+  //   from: "Alin",
+  //   text: "One more minute",
+  //   createdAt: 123123
+  // });
 
   socket.on("createMessage", message => {
     console.log("createMessage", message);
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   // socket.on("createEmail", newEmail => {
